82da8383d99ed2b7526ea51034e7799a2e94e4d4
KELLYPEARL <pearlkakes@gmail.com>
2022-10-16 06:34:50 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
  #include <stdio.h>
-
 /**
- * main - Entry point
+ * main - print a line of code using puts
  *
- * Return: Always 0 (Success)
+ * Return: 0
  */
 
 int main(void)
-{
-    puts("\"Programming is liike building a multilingual puzzle");
-    return (0);
-
+{puts("\"Programming is liike building a multilingual puzzle");
+return (0);
 }
