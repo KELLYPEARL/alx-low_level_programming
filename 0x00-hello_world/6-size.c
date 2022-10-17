898644813befbbdff5ffb03dd5cc85bb4d2c2b0f
KELLYPEARL <pearlkakes@gmail.com>
2022-10-17 02:56:21 -0700

    TASK O for variables

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-printf("Size of a char: %lu byte(s)\n", sizeof(1 byte(s)));
-printf("Size of a int: %lu byte(s)\n", sizeof(4 byte(s)));
-printf("Size of a long int: %lu byte(s)\n", sizeof(4 byte(s)));
-printf("Size of a long long int: %lu byte(s)\n", sizeof(8 byte(s)));
-printf("Size of a float: %lu byte(s)\n", sizeof(4 byte(s)))
+printf("Size of a char: %lu byte(s)\n", sizeof(1 bytes));
+printf("Size of a int: %lu byte(s)\n", sizeof(4 bytes));
+printf("Size of a long int: %lu byte(s)\n", sizeof(4 bytes));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(8 'bytes'));
+printf("Size of a float: %lu byte(s)\n", sizeof(4 'bytes'))
 return (0);
 }
 
